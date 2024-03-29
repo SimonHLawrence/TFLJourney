f7d43c8577eb65ebc606d28ad3b9be8842ed1dac
Simon Lawrence <simon.h.lawrence@gmail.com>
2024-03-29 12:48:14 +0000

    Rename project

--- Package.swift
+++ Package.swift
@@ -1,28 +1,33 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "TFLAPI",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TFLAPI",
-            targets: ["TFLAPI"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TFLAPI",
-            dependencies: []),
-        .testTarget(
-            name: "TFLAPITests",
-            dependencies: ["TFLAPI"]),
-    ]
+  name: "TFLJourney",
+  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+  products: [
+    .library(
+      name: "TFLJourney",
+      targets: ["TFLJourney"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "TFLJourney",
+      dependencies: [
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+      ],
+      plugins: [
+        .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+      ]),
+    .testTarget(
+      name: "TFLJourneyTests",
+      dependencies: ["TFLJourney"]),
+  ]
 )
---
+++ Sources/TFLJourney/TFLJourney.swift
@@ -0,0 +1,8 @@
+//
+//  TFLJourney.swift
+//  TFLJourney
+//
+//  Created by Simon Lawrence on 29/03/2024.
+//
+
+import Foundation
