be521d1b73916662d33312f613857759e373af5c
Simon Lawrence <simon.h.lawrence@gmail.com>
2024-03-29 12:50:13 +0000

    Initial add generated OpenAPI files

---
+++ Tests/TFLJourneyTests/TFLAPITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TFLJourney
+
+final class TFLAPITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TFLAPI().text, "Hello, World!")
+    }
+}
